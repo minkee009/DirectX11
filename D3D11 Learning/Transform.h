aaa22ec80f453f98e4e7b16229185650b3e8a78e
minkee009 <minkee5004@gmail.com>
2025-09-12 10:14:01 +0900

    DirectXTK 추가

--- D3D11 Learning/Transform.h
+++ D3D11 Learning/Transform.h
@@ -1,50 +1,63 @@
 #pragma once
-#include <DirectXMath.h>
+#include <SimpleMath.h>
 #include <vector>
 
-using namespace DirectX;
+using namespace DirectX::SimpleMath;
 
 namespace MyEngine
 {
 	class Transform
 	{
 	private:
-		XMFLOAT3 m_localPos;
-		XMFLOAT4 m_localRot;
-		XMFLOAT3 m_localScale;
+		Vector3 m_localPos;
+		Quaternion m_localRot;
+		Vector3 m_localScale;
 
 		mutable bool m_isMatrixDirty;
 
 		Transform* m_parent;
 		std::vector<Transform*> m_childs;
 
-		mutable XMMATRIX m_cachedMatrix;
+		mutable Matrix m_cachedMatrix;
 
 		void AddChild(Transform* child);
 		void RemoveChild(Transform* child);
 		void MarkDirty();
 	public:
-		XMMATRIX& GetLocalMatrix() const;
-		XMMATRIX& GetWorldMatrix() const;
+		Matrix& GetLocalMatrix() const;
+		Matrix& GetWorldMatrix() const;
 
-		XMFLOAT3& GetLocalPosition() const;
-		XMFLOAT4& GetLocalRotation() const;
-		XMFLOAT3& GetLocalScale() const;
+		Vector3& GetLocalPosition() const;
+		Quaternion& GetLocalRotation() const;
+		Vector3& GetLocalScale() const;
 
-		XMFLOAT3& GetWorldPosition() const;
-		XMFLOAT4& GetWorldRotation() const;
-		XMFLOAT3& GetWorldScale() const;
+		Vector3& GetWorldPosition() const;
+		Quaternion& GetWorldRotation() const;
+		Vector3& GetWorldScale() const;
 
-		void SetLocalPosition(XMFLOAT3 pos);
+		void SetWorldPosition(Vector3 pos);
+		void SetWorldPosition(float x, float y, float z);
+				
+		void SetWorldRotation(Quaternion rot);
+		void SetWorldRotation(float x, float y, float z, float w);
+				
+		void SetWorldEulerRotation(Vector3 rot); // use degree (0 ~ 360)
+		void SetWorldEulerRotation(float x, float y, float z); // use degree (0 ~ 360)
+				
+		void SetWorldScale(Vector3 scale);
+		void SetWorldScale(float x, float y, float z);
+
+
+		void SetLocalPosition(Vector3 pos);
 		void SetLocalPosition(float x, float y, float z);
 
-		void SetLocalRotation(XMFLOAT4 rot);
-		void SetLocalRotation(float w, float x, float y, float z);
+		void SetLocalRotation(Quaternion rot);
+		void SetLocalRotation(float x, float y, float z, float w);
 
-		void SetLocalEulerRotation(XMFLOAT3 rot); // use degree (0 ~ 360)
+		void SetLocalEulerRotation(Vector3 rot); // use degree (0 ~ 360)
 		void SetLocalEulerRotation(float x, float y, float z); // use degree (0 ~ 360)
 
-		void SetLocalScale(XMFLOAT3 scale);
+		void SetLocalScale(Vector3 scale);
 		void SetLocalScale(float x, float y, float z);
 
 		inline void SetParent(Transform* parent) { m_parent = parent; }
