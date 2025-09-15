5ff98dcd041cec60947e66857965149b2635d6e8
minkee009 <minkee5004@gmail.com>
2025-09-15 10:08:20 +0900

    Update MyImGui.cpp

--- D3D11 Learning/MyImGui.cpp
+++ D3D11 Learning/MyImGui.cpp
@@ -11,7 +11,7 @@
 
 bool MyEngine::MyImGui::Initialize(MyD3DContext* myContext)
 {
-	m_d3dContext = myContext;
+    m_d3dContext = myContext;
 
     this->m_hWnd = m_d3dContext->m_hWnd;
     this->m_pDevice = m_d3dContext->m_pd3dDevice.Get();
@@ -19,25 +19,25 @@ bool MyEngine::MyImGui::Initialize(MyD3DContext* myContext)
 
     // 코어 컨텍스트 생성
     if (!ImGui::CreateContext()) {
-        return false; 
+        return false;
     }
     m_isImGuiInit = true;
 
     ImGuiIO& io = ImGui::GetIO();
     io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // 키보드 내비게이션 활성화
-	io.Fonts->AddFontFromFileTTF("C:/Windows/Fonts/malgun.ttf", 16.0f, NULL, io.Fonts->GetGlyphRangesKorean()); // 한글 폰트 설정
+    io.Fonts->AddFontFromFileTTF("C:/Windows/Fonts/malgun.ttf", 16.0f, NULL, io.Fonts->GetGlyphRangesKorean()); // 한글 폰트 설정
 
     // 스타일 설정
     ImGui::StyleColorsDark();
     ImGuiStyle& style = ImGui::GetStyle();
-    
-	// 둥근 모서리 설정
+
+    // 둥근 모서리 설정
     style.FrameRounding = 6.0f;
     style.WindowRounding = 10.0f;
-	style.GrabRounding = 6.0f;
-	style.ScrollbarRounding = 6.0f;
+    style.GrabRounding = 6.0f;
+    style.ScrollbarRounding = 6.0f;
 
-	// 색상 설정
+    // 색상 설정
     //style.Colors[ImGuiCol_Text] = ImVec4(0.90f, 0.90f, 0.90f, 0.90f);
     //style.Colors[ImGuiCol_WindowBg] = ImVec4(0.09f, 0.09f, 0.15f, 1.00f);
     //style.Colors[ImGuiCol_FrameBg] = ImVec4(0.00f, 1.00f, 0.01f, 1.00f);
@@ -67,7 +67,7 @@ bool MyEngine::MyImGui::Initialize(MyD3DContext* myContext)
 void MyEngine::MyImGui::BeginFrame()
 {
     RECT clientRect;
-    GetClientRect(m_hWnd,&clientRect);
+    GetClientRect(m_hWnd, &clientRect);
 
     // 너비와 높이를 명시적으로 계산
     LONG width = clientRect.right - clientRect.left;
@@ -97,7 +97,7 @@ void MyEngine::MyImGui::Update()
     ImGui::Text("  Y : %f", m_d3dContext->m_pCamera->GetTransform()->GetLocalPosition().y);
     ImGui::Text("  Z : %f", m_d3dContext->m_pCamera->GetTransform()->GetLocalPosition().z);
 
-	Vector3 euler = m_d3dContext->m_pCamera->GetTransform()->GetLocalRotation().ToEuler();
+    Vector3 euler = m_d3dContext->m_pCamera->GetTransform()->GetLocalRotation().ToEuler();
 
     ImGui::Text(u8"회전");
     ImGui::Text("  X : %f", XMConvertToDegrees(euler.x));
@@ -106,11 +106,11 @@ void MyEngine::MyImGui::Update()
 
     ImGui::Separator();
 
-	constexpr float defFov = 75.0f;
+    constexpr float defFov = 75.0f;
     static float fov = defFov;
     ImGui::Text(u8"시야각");
     ImGui::SliderFloat("##FOV", &fov, 60.0f, 120.0f);
-	m_d3dContext->m_pCamera->SetFOV(fov);
+    m_d3dContext->m_pCamera->SetFOV(fov);
     ImGui::SameLine();
     if (ImGui::Button(u8"초기화")) {
         fov = defFov;
@@ -139,9 +139,9 @@ void MyEngine::MyImGui::Update()
     ImGui::End();
 
     ImGui::SetNextWindowPos(ImVec2(5, 390), ImGuiCond_Once);
-    ImGui::SetNextWindowSize(ImVec2(310, 180), ImGuiCond_Once);
+    ImGui::SetNextWindowSize(ImVec2(220, 180), ImGuiCond_Once);
 
-    ImGui::Begin(u8"오브젝트 상태",nullptr, ImGuiWindowFlags_NoResize);
+    ImGui::Begin(u8"오브젝트 상태", nullptr, ImGuiWindowFlags_NoResize);
 
     auto obj1 = m_d3dContext->m_sceneObjects[0].get();
     auto obj2 = m_d3dContext->m_sceneObjects[1].get();
@@ -157,9 +157,12 @@ void MyEngine::MyImGui::Update()
     static Vector3 obj3_pos = { 4.0f, 0.0f, 0.0f };
 
     ImGui::Text(u8"오브젝트 1 월드 위치");
-    ImGui::DragFloat3("##obj1_pos",&obj1_pos.x);
-    ImGui::SameLine();
-    if (ImGui::Button(u8"적용")) {
+    if (ImGui::DragFloat3("##obj1_pos", &obj1_pos.x))
+    {
+        obj1->SetLocalPosition(obj1_pos);
+    }
+    if (ImGui::IsItemDeactivatedAfterEdit())
+    {
         obj1->SetLocalPosition(obj1_pos);
     }
     ImGui::SameLine();
@@ -169,9 +172,12 @@ void MyEngine::MyImGui::Update()
     }
 
     ImGui::Text(u8"오브젝트 2 로컬 위치");
-    ImGui::DragFloat3("##obj2_pos", &obj2_pos.x);
-    ImGui::SameLine();
-    if (ImGui::Button(u8"적용##2")) {
+    if (ImGui::DragFloat3("##obj2_pos", &obj2_pos.x))
+    {
+        obj2->SetLocalPosition(obj2_pos);
+    }
+    if (ImGui::IsItemDeactivatedAfterEdit())
+    {
         obj2->SetLocalPosition(obj2_pos);
     }
     ImGui::SameLine();
@@ -181,9 +187,12 @@ void MyEngine::MyImGui::Update()
     }
 
     ImGui::Text(u8"오브젝트 3 로컬 위치");
-    ImGui::DragFloat3("##obj3_pos", &obj3_pos.x);
-    ImGui::SameLine();
-    if (ImGui::Button(u8"적용##3")) {
+    if (ImGui::DragFloat3("##obj3_pos", &obj3_pos.x))
+    {
+        obj3->SetLocalPosition(obj3_pos);
+    }
+    if (ImGui::IsItemDeactivatedAfterEdit())
+    {
         obj3->SetLocalPosition(obj3_pos);
     }
     ImGui::SameLine();
@@ -191,7 +200,6 @@ void MyEngine::MyImGui::Update()
         obj3_pos = obj3_defpos;
         obj3->SetLocalPosition(obj3_defpos);
     }
-
 
     ImGui::End();
 }
@@ -204,11 +212,11 @@ void MyEngine::MyImGui::Render()
 
 void MyEngine::MyImGui::Uninitialize()
 {
-    if(m_isD3D11BackendInit)
+    if (m_isD3D11BackendInit)
         ImGui_ImplDX11_Shutdown();
-    if(m_isWin32BackendInit)
+    if (m_isWin32BackendInit)
         ImGui_ImplWin32_Shutdown();
-    if(m_isImGuiInit)
+    if (m_isImGuiInit)
         ImGui::DestroyContext();
 
     m_isImGuiInit = false;
