bb01cd3753c799698fba76a873d11812c5a55397
minkee009 <minkee5004@gmail.com>
2025-09-22 01:57:20 +0900

    Update testPS.hlsl

--- D3D11 Learning/Resources/Shaders/testPS.hlsl
+++ D3D11 Learning/Resources/Shaders/testPS.hlsl
@@ -1,13 +1,6 @@
 Texture2D txDiffuse : register(t0);
 SamplerState samLinear : register(s0);
 
-cbuffer ConstantBuffer : register(b0)
-{
-    matrix World;
-    matrix View;
-    matrix Projection;
-}
-
 struct VS_OUTPUT
 {
     float4 Pos : SV_POSITION;
